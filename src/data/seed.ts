b7dc6dec95f76218a116566e9d92958661f0f6e5
cluj1313 <cluj1313@gmail.com>
2026-09-22 06:40:39 +0300

    fix: export admin identity constants from seed

--- src/data/seed.ts
+++ src/data/seed.ts
@@ -1,6 +1,9 @@
 export {
   DEMO_ADMIN_PHONE,
   DEMO_ADMIN_PASSWORD,
+  DEMO_ADMIN_EMAIL,
+  DEMO_ADMIN_NAME,
+  DEMO_ADMIN_PHONE_DISPLAY,
   seedUsers,
   seedJobs,
 } from './seedJobs';
--- src/data/seedJobs.ts
+++ src/data/seedJobs.ts
@@ -1,6 +1,9 @@
 export {
   DEMO_ADMIN_PHONE,
   DEMO_ADMIN_PASSWORD,
+  DEMO_ADMIN_EMAIL,
+  DEMO_ADMIN_NAME,
+  DEMO_ADMIN_PHONE_DISPLAY,
   seedUsers,
 } from './seedUsersData';
 export { seedJobs } from './seedJobsData';
