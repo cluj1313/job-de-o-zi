4fd7da2705b7bfcf8bbac9cf4190a95c5e42f32a
cluj1313 <cluj1313@gmail.com>
2026-09-22 03:41:38 +0300

    assets: home collage assembler (photo_home)

---
+++ src/assets/photo_home.ts
@@ -0,0 +1,4 @@
+import { p0 } from './photo_home_p0';
+import { p1 } from './photo_home_p1';
+/** User warm jobs+pocket-watch collage */
+export const photoHome = `data:image/jpeg;base64,${p0}${p1}`;
